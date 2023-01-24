317015186c94340ed142083802b04e3d6aa87a03
Ethan Whitaker <ethan@orbsix.com>
2023-01-24 12:01:24 -0700

    Added Variable and Move scripts

---
+++ First Prototype/Assets/Scripts/Variables.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // [Access Modifier] [Data-Type/Type] [Identifier/name] [Access Operator =] [Value]
+    
+    public string herosName = "Batman";
+    private int herosAge = 30;
+    public float heroHeight = 6.2f;
+    public char heroLetter = 'B';
+    private double heroNetWorth = 100.45d;
+    public bool isHeroInv = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
