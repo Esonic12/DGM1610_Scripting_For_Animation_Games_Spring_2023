7475d81279251ece7f6b7e091839eee585bd757d
Ethan Whitaker <ethan@orbsix.com>
2023-02-16 17:16:58 -0700

    Begun Making Detection Collision

---
+++ UFO Defense Force Game/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+
+    private ScoreManager scoreManager;
+    public int scoreToGive;
+    public ParticleSystem explosionParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("LaserBolt"))
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+
+        Explosion();
+        scoreManager.IncreaseScore(scoreToGive);
+    }
+
+    void Explosion()
+    {
+        Instantiate(explosionParticle, transform.position, transform.rotation);
+    }
+}
