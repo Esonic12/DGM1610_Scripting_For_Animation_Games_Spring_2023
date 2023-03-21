84340dc85b836a4127352e1e6e9f4a1008c989d6
Ethan Whitaker <ethan@orbsix.com>
2023-03-21 17:47:23 -0600

    made an enemy that moves

---
+++ 2D Platformer Game Spring 2023/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public float rayDistance;
+    private bool isMovingRight = true;
+    public Transform groundDetection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, rayDistance);
+
+        if(groundInfo.collider == false)
+        {
+            if(isMovingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                isMovingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                isMovingRight = true;
+            }
+        }
+    }
+}
